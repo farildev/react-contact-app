fd05b3c04d0a20fae3569357b5ae5abc17fde280
farildev <memmedovfaril@gmail.com>
2023-04-01 12:50:54 +0400

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Contact from "./components/Contacts"
 
 function App() {
   return (
---
+++ src/components/Contacts/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from "./Form"
+import List from "./List"
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
