e70e8c799f4d1f76ef8067db0b57d325ff115dd1
farildev <memmedovfaril@gmail.com>
2023-04-03 18:54:04 +0400

    UPDATE

--- src/components/Contacts/List/ListEl.jsx
+++ src/components/Contacts/List/ListEl.jsx
@@ -1,6 +1,17 @@
-import React  from 'react'
+import React , {useState}  from 'react'
 import "./list.css"
 function ListEl({ contacts })  {
+
+  const [filterText , setFilterText] = useState('');
+
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) => 
+    item[key]
+    .toString()
+    .toLowerCase()
+    .includes(filterText.toLocaleLowerCase())
+    )
+  })
 
   //Contacts propsu vasitəsilə contact dəyərlərini parent elementdən alıb list componentində view-a çıxardırıq.(Arraylar üçün map metodu)
   //Contacts map edildikdə hər döngüdə bizə bir contact elementi qaytarır.
@@ -11,8 +22,13 @@ function ListEl({ contacts })  {
       <div className="list__header">
         <h4>My Contacts</h4>
       </div>
+
+      <div className="filter">
+        <input value={filterText} placeholder='Search contacts' onChange={(e) => setFilterText(e.target.value)} />
+      </div>
+
       <ul className="list__box">
-        {contacts.map((contact,index) => 
+        {filtered.map((contact,index) => 
         (<li className='list__item' key={index}>
           <span>{contact.fullname}</span>
           <span>{contact.phone}</span>
